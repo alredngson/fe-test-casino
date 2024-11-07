7efa3d0b468615a6d907c32ac99ef1d860dcd59f
Alfred Tingson <alredngson@gmail.com>
2024-11-08 07:17:26 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./styles/style.css";
+
+const App = () => <>App</>;
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(<App />);
