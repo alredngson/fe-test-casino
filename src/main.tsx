ad0089add14721b6a9a8b1388318d710bc7375b3
Alfred Tingson <alredngson@gmail.com>
2024-11-08 07:20:54 +0800

    feat(UI): updated assets. added notif text and mid-nav

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,30 @@
 import React, { type ReactElement } from "react";
 import { createRoot } from "react-dom/client";
 
-import FavoritesIcon from "./assets/svg_icons/FAVE.svg";
 import ThreeBarIcon from "./assets/svg_icons/3BAR.svg";
 import Fun88Icon from "./assets/logos/F1M5_Logo.webp";
 import UserHeadIcon from "./assets/svg_icons/userhead.svg";
 import WalletIcon from "./assets/svg_icons/wallet.svg";
 import CarouselBanner from "./assets/Frame 18.webp";
+import BellIcon from "./assets/svg_icons/bell-svgrepo-com 1.svg";
+import SearchIcon from "./assets/svg_icons/search-925.svg";
+import FireIcon from "./assets/svg_icons/fire-svgrepo-com 1.svg";
+import NewIcon from "./assets/svg_icons/NEW.svg";
+import SlotMachineIcon from "./assets/svg_icons/slotmachine.svg";
+import DealerIcon from "./assets/svg_icons/casino-dealer-illustration-2-svgrepo-com 1.svg";
+import JackpotIcon from "./assets/svg_icons/jackpots.svg";
+import SportsIcon from "./assets/svg_icons/SPORTS.svg";
+import FavoritesIcon from "./assets/svg_icons/FAVE.svg";
+import InviteIcon from "./assets/svg_icons/INVITE.svg";
+import LiveIcon from "./assets/svg_icons/LIVE.svg";
+import CashierIcon from "./assets/svg_icons/CASHIER.svg";
 
 import "./styles/style.css";
 
 const App = (): ReactElement => (
   <>
     <div id="app">
+      {/* top navbar */}
       <div className="container-fluid py-3">
         <div id="top-nav" className="bg-light">
           <div className="row text-center flex-nowrap">
@@ -29,7 +41,10 @@ const App = (): ReactElement => (
               <div className="d-flex align-items-center pe-2">
                 <img src={UserHeadIcon} alt="UserHead Icon" className="mb-1" />
               </div>
-              <div className="text-nowrap icon-label fw-bolder wallet-label pe-2 mx-2">
+              <div
+                id="wallet-label"
+                className="text-nowrap icon-label fw-bolder pe-2 mx-2"
+              >
                 $1990.6
               </div>
               <div className="d-flex align-items-center">
@@ -40,7 +55,8 @@ const App = (): ReactElement => (
         </div>
       </div>
 
-      <div className="container-fluid px-3">
+      {/* banner */}
+      <div className="container-fluid px-3 py-1">
         <div
           id="carousel-banner"
           className="carousel slide"
@@ -65,27 +81,81 @@ const App = (): ReactElement => (
         </div>
       </div>
 
+      {/* notif text */}
+      <div className="container-fluid pt-1">
+        <div id="notif-text" className="bg-light">
+          <div className="row text-center flex-nowrap">
+            <div className="col d-flex flex-row align-items-center">
+              <div id="notif-label" className="pe-1 align-items-center">
+                <img src={BellIcon} alt="Bell Icon" className="mb-1" />
+              </div>
+              <div className="text-nowrap notif-label">
+                LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ELIT EMUR
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* mid nav */}
+      <div className="container-fluid py-1">
+        <div id="middle-nav" className="bg-light py-2">
+          <div className="row text-center flex-nowrap">
+            <div
+              id="search-icon-div"
+              className="col d-flex flex-column align-items-center"
+            >
+              <img src={SearchIcon} alt="Search Icon" />
+              <div id="search-icon-label" className="text-nowrap icon-label">
+                SEARCH
+              </div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FireIcon} alt="Start Icon" />
+              <div className="text-nowrap icon-label">START</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={NewIcon} alt="New Icon" />
+              <div className="text-nowrap icon-label">NEW</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={SlotMachineIcon} alt="Slot Icon" />
+              <div className="text-nowrap icon-label">SLOTS</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={DealerIcon} alt="Live Icon" />
+              <div className="text-nowrap icon-label">LIVE</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={JackpotIcon} alt="Jackpot Icon" />
+              <div className="text-nowrap icon-label">JACKPOT</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* bottom nav */}
       <div className="container-fluid fixed-bottom">
         <div id="bottom-nav" className="bg-light py-2">
           <div className="row text-center flex-nowrap">
             <div className="col d-flex flex-column align-items-center">
-              <img src={FavoritesIcon} alt="Sports Icon" className="mb-1" />
+              <img src={SportsIcon} alt="Sports Icon" />
               <div className="text-nowrap icon-label">SPORTS</div>
             </div>
             <div className="col d-flex flex-column align-items-center">
-              <img src={FavoritesIcon} alt="Favorites Icon" className="mb-1" />
+              <img src={FavoritesIcon} alt="Favorites Icon" />
               <div className="text-nowrap icon-label">FAVORITES</div>
             </div>
             <div className="col d-flex flex-column align-items-center">
-              <img src={FavoritesIcon} alt="Invite Icon" className="mb-1" />
+              <img src={InviteIcon} alt="Invite Icon" />
               <div className="text-nowrap icon-label">INVITE</div>
             </div>
             <div className="col d-flex flex-column align-items-center">
-              <img src={FavoritesIcon} alt="Casino Icon" className="mb-1" />
+              <img src={LiveIcon} alt="Casino Icon" />
               <div className="text-nowrap icon-label">CASINO LIVE</div>
             </div>
             <div className="col d-flex flex-column align-items-center">
-              <img src={FavoritesIcon} alt="Cashier Icon" className="mb-1" />
+              <img src={CashierIcon} alt="Cashier Icon" />
               <div className="text-nowrap icon-label">CASHIER</div>
             </div>
           </div>
