b504180b1f351a6f87a9dd3368cfe7905ec7b4bf
Alfred Tingson <alredngson@gmail.com>
2024-11-08 07:19:29 +0800

    feat(UI): bottom navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { type ReactElement } from "react";
 import { createRoot } from "react-dom/client";
+
+import FavoritesIcon from "./assets/svg_icons/FAVE.svg";
 
 import "./styles/style.css";
 
-const App = () => <>App</>;
+const App = (): ReactElement => (
+  <>
+    <div className="container-fluid fixed-bottom">
+      <div id="bottom-nav" className="bg-light py-2">
+        <div className="row text-center flex-nowrap">
+          <div className="col d-flex flex-column align-items-center">
+            <img src={FavoritesIcon} alt="Sports Icon" className="mb-1" />
+            <div className="text-nowrap nav-icon-label">SPORTS</div>
+          </div>
+          <div className="col d-flex flex-column align-items-center">
+            <img src={FavoritesIcon} alt="Favorites Icon" className="mb-1" />
+            <div className="text-nowrap nav-icon-label">FAVORITES</div>
+          </div>
+          <div className="col d-flex flex-column align-items-center">
+            <img src={FavoritesIcon} alt="Invite Icon" className="mb-1" />
+            <div className="text-nowrap nav-icon-label">INVITE</div>
+          </div>
+          <div className="col d-flex flex-column align-items-center">
+            <img src={FavoritesIcon} alt="Casino Icon" className="mb-1" />
+            <div className="text-nowrap nav-icon-label">CASINO LIVE</div>
+          </div>
+          <div className="col d-flex flex-column align-items-center">
+            <img src={FavoritesIcon} alt="Cashier Icon" className="mb-1" />
+            <div className="text-nowrap nav-icon-label">CASHIER</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
