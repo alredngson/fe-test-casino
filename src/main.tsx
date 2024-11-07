0df76d5a8f7764a1fa0d3a9e9202bfffce9b111e
Alfred Tingson <alredngson@gmail.com>
2024-11-08 07:19:56 +0800

    feat(UI): top navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,33 +2,66 @@ import React, { type ReactElement } from "react";
 import { createRoot } from "react-dom/client";
 
 import FavoritesIcon from "./assets/svg_icons/FAVE.svg";
+import ThreeBarIcon from "./assets/svg_icons/3BAR.svg";
+import Fun88Icon from "./assets/logos/F1M5_Logo.webp";
+import UserHeadIcon from "./assets/svg_icons/userhead.svg";
+import WalletIcon from "./assets/svg_icons/wallet.svg";
 
 import "./styles/style.css";
 
 const App = (): ReactElement => (
   <>
-    <div className="container-fluid fixed-bottom">
-      <div id="bottom-nav" className="bg-light py-2">
-        <div className="row text-center flex-nowrap">
-          <div className="col d-flex flex-column align-items-center">
-            <img src={FavoritesIcon} alt="Sports Icon" className="mb-1" />
-            <div className="text-nowrap nav-icon-label">SPORTS</div>
+    <div id="app">
+      <div className="container-fluid fixed-top">
+        <div id="top-nav" className="bg-light py-2">
+          <div className="row text-center flex-nowrap">
+            <div className="col d-flex flex-row">
+              <div>
+                <img src={ThreeBarIcon} alt="ThreeBar Icon" className="mb-1" />
+              </div>
+              <div className="px-2">
+                <img src={Fun88Icon} alt="Fun88 Icon" className="mb-1" />
+              </div>
+            </div>
+
+            <div className="col d-flex flex-row-reverse align-items-center px-1">
+              <div className="d-flex align-items-center pe-2">
+                <img src={UserHeadIcon} alt="UserHead Icon" className="mb-1" />
+              </div>
+              <div className="text-nowrap icon-label fw-bolder wallet-label pe-2 mx-2">
+                $1990.6
+              </div>
+              <div className="d-flex align-items-center">
+                <img src={WalletIcon} alt="Wallet Icon" className="mb-1" />
+              </div>
+            </div>
           </div>
-          <div className="col d-flex flex-column align-items-center">
-            <img src={FavoritesIcon} alt="Favorites Icon" className="mb-1" />
-            <div className="text-nowrap nav-icon-label">FAVORITES</div>
-          </div>
-          <div className="col d-flex flex-column align-items-center">
-            <img src={FavoritesIcon} alt="Invite Icon" className="mb-1" />
-            <div className="text-nowrap nav-icon-label">INVITE</div>
-          </div>
-          <div className="col d-flex flex-column align-items-center">
-            <img src={FavoritesIcon} alt="Casino Icon" className="mb-1" />
-            <div className="text-nowrap nav-icon-label">CASINO LIVE</div>
-          </div>
-          <div className="col d-flex flex-column align-items-center">
-            <img src={FavoritesIcon} alt="Cashier Icon" className="mb-1" />
-            <div className="text-nowrap nav-icon-label">CASHIER</div>
+        </div>
+      </div>
+
+      <div className="container-fluid fixed-bottom">
+        <div id="bottom-nav" className="bg-light py-2">
+          <div className="row text-center flex-nowrap">
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FavoritesIcon} alt="Sports Icon" className="mb-1" />
+              <div className="text-nowrap icon-label">SPORTS</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FavoritesIcon} alt="Favorites Icon" className="mb-1" />
+              <div className="text-nowrap icon-label">FAVORITES</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FavoritesIcon} alt="Invite Icon" className="mb-1" />
+              <div className="text-nowrap icon-label">INVITE</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FavoritesIcon} alt="Casino Icon" className="mb-1" />
+              <div className="text-nowrap icon-label">CASINO LIVE</div>
+            </div>
+            <div className="col d-flex flex-column align-items-center">
+              <img src={FavoritesIcon} alt="Cashier Icon" className="mb-1" />
+              <div className="text-nowrap icon-label">CASHIER</div>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/types/assets.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg" {
+  const svgUrl: string;
+  export default svgUrl;
+}
+
+declare module "*.webp" {
+  const webpUrl: string;
+  export default webpUrl;
+}
