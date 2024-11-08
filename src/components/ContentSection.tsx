f20d26120dd0649680365eb8bab8f7523201b43c
Alfred Tingson <alredngson@gmail.com>
2024-11-08 14:57:47 +0800

    feat(api): structure mock data

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -1,32 +1,65 @@
-import React, { type ReactElement } from "react";
+import React, { type ReactElement, useState, useEffect } from "react";
 
-import { BadWolfIcon } from "../utils/icons";
+import { games, providers } from "../utils/data";
 
-const ContentSection = (): ReactElement => (
-  <>
+interface Game {
+  id: string;
+  name: string;
+  img: string;
+  provider: string;
+  tags: string[];
+}
+interface Provider {
+  id: string;
+  name: string;
+  img: string;
+}
+
+const ContentSection = (): ReactElement => {
+  const [game, setGame] = useState<Game[]>([]);
+  const [provider, setProvider] = useState<Provider[]>([]);
+
+  const fetchGames = async () => {
+    const gameData: Game[] = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(games);
+      }, 3000);
+    });
+    setGame(gameData);
+  };
+
+  const fetchProviders = async () => {
+    const providerData: Provider[] = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(providers);
+      }, 3000);
+    });
+    setProvider(providerData);
+  };
+
+  useEffect(() => {
+    fetchGames();
+    fetchProviders();
+  }, []);
+
+  console.log("game state: ", game);
+  console.log("provider state: ", provider);
+
+  return (
     <div className="container-fluid">
       <div className="row row-cols-4">
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
-        <div className="col p-1">
-          <img src={BadWolfIcon} className="card-img-top" alt="Game Icon" />
-        </div>
+        {games.map((game) => (
+          <div key={game.id} className="col p-1">
+            <img
+              src={game.img}
+              className="card-img-top"
+              alt={`${game.name} Icon`}
+            />
+          </div>
+        ))}
       </div>
     </div>
-  </>
-);
+  );
+};
 
 export default ContentSection;
--- src/utils/icons.ts
+++ src/utils/icons.ts
@@ -1,39 +1,98 @@
-import ThreeBarIcon from "../assets/svg_icons/3BAR.svg";
 import Fun88Icon from "../assets/logos/F1M5_Logo.webp";
+import EveryMatrixIcon from "../assets/logos/EM.webp";
+import EvolutionIcon from "../assets/logos/EVO.webp";
+import ExpanseIcon from "../assets/logos/EXPANSE.webp";
+import EzugiIcon from "../assets/logos/EZG.webp";
+import GameArtIcon from "../assets/logos/GAMEART.webp";
+import HabeneroIcon from "../assets/logos/HAB.webp";
+import HackSawGamingIcon from "../assets/logos/HACKSAW.webp";
+import InbetGamesIcon from "../assets/logos/INBET.webp";
+import MplayIcon from "../assets/logos/MPLAY.webp";
+import NetEntIcon from "../assets/logos/NETENT (1).webp";
+import PgIcon from "../assets/logos/PGSOFT.webp";
+import PlayNgoIcon from "../assets/logos/PNG.webp";
+import PragmaticPlayIcon from "../assets/logos/PRAGMATICPLAY.webp";
+import PlaysonIcon from "../assets/logos/PS.webp";
+import PlayTechIcon from "../assets/logos/PT.webp";
+import RedTigerIcon from "../assets/logos/REDTIGER.webp";
+import RelaxGamingIcon from "../assets/logos/RELAX.webp";
+
+import BadWolfIcon from "../assets/game_icons/Group 72.webp";
+import PrideOfPersiaIcon from "../assets/game_icons/gpas_etpop_pop.webp.webp";
+import SugarRushIcon from "../assets/game_icons/Group 70.webp";
+import ShaolinCrewIcon from "../assets/game_icons/Group 75.webp";
+import BookOfEgyptIcon from "../assets/game_icons/Group 83.webp";
+import PiratesPowerIcon from "../assets/game_icons/Group 84.webp";
+import CrocodileBlitzIcon from "../assets/game_icons/Group 85.webp";
+import AnacondaWildIcon from "../assets/game_icons/Group 86.webp";
+import MayaJackpotIcon from "../assets/game_icons/Group 87.webp";
+import BeachLifeIcon from "../assets/game_icons/Group 88.webp";
+import AztecaIcon from "../assets/game_icons/Group 94.webp";
+import IncaJackpotIcon from "../assets/game_icons/sw_ijp.webp.webp";
+
+import CarouselBannerIcon from "../assets/Frame 18.webp";
+
+import ThreeBarIcon from "../assets/svg_icons/3BAR.svg";
+import BellIcon from "../assets/svg_icons/bell-svgrepo-com 1.svg";
+import CashierIcon from "../assets/svg_icons/CASHIER.svg";
+import DealerIcon from "../assets/svg_icons/casino-dealer-illustration-2-svgrepo-com 1.svg";
+import FavoritesIcon from "../assets/svg_icons/FAVE.svg";
+import FireIcon from "../assets/svg_icons/fire-svgrepo-com 1.svg";
+import InviteIcon from "../assets/svg_icons/INVITE.svg";
+import JackpotIcon from "../assets/svg_icons/jackpots.svg";
+import LiveIcon from "../assets/svg_icons/LIVE.svg";
+import NewIcon from "../assets/svg_icons/NEW.svg";
+import SearchIcon from "../assets/svg_icons/search-925.svg";
+import SlotMachineIcon from "../assets/svg_icons/slotmachine.svg";
+import SportsIcon from "../assets/svg_icons/SPORTS.svg";
 import UserHeadIcon from "../assets/svg_icons/userhead.svg";
 import WalletIcon from "../assets/svg_icons/wallet.svg";
-import CarouselBannerIcon from "../assets/Frame 18.webp";
-import BellIcon from "../assets/svg_icons/bell-svgrepo-com 1.svg";
-import SearchIcon from "../assets/svg_icons/search-925.svg";
-import FireIcon from "../assets/svg_icons/fire-svgrepo-com 1.svg";
-import NewIcon from "../assets/svg_icons/NEW.svg";
-import SlotMachineIcon from "../assets/svg_icons/slotmachine.svg";
-import DealerIcon from "../assets/svg_icons/casino-dealer-illustration-2-svgrepo-com 1.svg";
-import JackpotIcon from "../assets/svg_icons/jackpots.svg";
-import SportsIcon from "../assets/svg_icons/SPORTS.svg";
-import FavoritesIcon from "../assets/svg_icons/FAVE.svg";
-import InviteIcon from "../assets/svg_icons/INVITE.svg";
-import LiveIcon from "../assets/svg_icons/LIVE.svg";
-import CashierIcon from "../assets/svg_icons/CASHIER.svg";
-import BadWolfIcon from "../assets/game_icons/Group 72.webp";
 
 export {
-  ThreeBarIcon,
   Fun88Icon,
-  UserHeadIcon,
-  WalletIcon,
+  EveryMatrixIcon,
+  EvolutionIcon,
+  ExpanseIcon,
+  EzugiIcon,
+  GameArtIcon,
+  HabeneroIcon,
+  HackSawGamingIcon,
+  InbetGamesIcon,
+  MplayIcon,
+  NetEntIcon,
+  PgIcon,
+  PlayNgoIcon,
+  PragmaticPlayIcon,
+  PlaysonIcon,
+  PlayTechIcon,
+  RedTigerIcon,
+  RelaxGamingIcon,
+  BadWolfIcon,
+  PrideOfPersiaIcon,
+  SugarRushIcon,
+  ShaolinCrewIcon,
+  BookOfEgyptIcon,
+  PiratesPowerIcon,
+  CrocodileBlitzIcon,
+  AnacondaWildIcon,
+  MayaJackpotIcon,
+  BeachLifeIcon,
+  AztecaIcon,
+  IncaJackpotIcon,
   CarouselBannerIcon,
+  ThreeBarIcon,
   BellIcon,
-  SearchIcon,
-  FireIcon,
-  NewIcon,
-  SlotMachineIcon,
-  DealerIcon,
-  JackpotIcon,
-  SportsIcon,
-  FavoritesIcon,
-  InviteIcon,
-  LiveIcon,
   CashierIcon,
-  BadWolfIcon
+  DealerIcon,
+  FavoritesIcon,
+  FireIcon,
+  InviteIcon,
+  JackpotIcon,
+  LiveIcon,
+  NewIcon,
+  SearchIcon,
+  SlotMachineIcon,
+  SportsIcon,
+  UserHeadIcon,
+  WalletIcon
 };
